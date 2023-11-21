04e4438335147f7f9e27fef9dd2384d62cbc7644
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-11-21 03:12:23 +0300

    Create bot.js
    Searches for the target site and goes to it

---
+++ bot.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Bot for Ecosia
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Selyunina Natalya
+// @match        https://www.ecosia.org/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+let button = document.getElementsByClassName("button")[1];
+let input = document.getElementsByName("q")[0];
+let links = document.links;
+let keywords = ["Каталог автомобилей", "Купить автомобиль", "Продажа автомобилей"];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+input.value = keyword;
+button.click();
+
+for (let i = 0; i < links.length; i++) {
+  if (links[i].href.includes("auto.ru")) {
+    console.log("Нашел строку " + links[i]);
+    let link = links[i];
+    link.click();
+    break;
+  }
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
